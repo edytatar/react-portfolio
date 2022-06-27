2f0cd9a585ed6df75bee377e9e6c57358fa7042c
edytatar <edytatar@gmail.com>
2022-06-27 13:46:00 -0500

    Added current position in about and added DevOps tile

--- src/components/About.js
+++ src/components/About.js
@@ -15,6 +15,10 @@ const technologies = [
     name: 'Tools',
     role: 'Jest, Apollo GraphQL, JSON Web Token, Postman',
   },
+  {
+    name: 'DevOps',
+    role: 'Jenkins, Artifactory, GitHub Enterprise',
+  },
 ]
 
 export default function About() {
@@ -87,10 +91,10 @@ export default function About() {
               </h2>
               <div className="mt-6 text-gray-500 space-y-6">
                 <p className="text-lg">
-                  I finished Northwestern University's Full Stack Coding Bootcamp in March 2022. In 2020, I graduated from DePaul University with a B.S in Marketing and a Minor in Graphic Design.
+                  I am currently a Junior DevOps Engineer at DocuSign. I finished Northwestern University's Full Stack Coding Bootcamp in March 2022. In 2020, I graduated from DePaul University with a B.S in Marketing and a Minor in Graphic Design.
                 </p>
                 <p className="text-lg">
-                  I am passionate about combining my design and coding skills to create innovative solutions for the community.  Creative problem solving and debugging are my jam. I am interested in Front End, Back End, and Full Stack engineering opportunities.
+                  I am passionate about combining my design and coding skills to create innovative solutions for the community.  Creative problem solving and debugging are my jam.
                 </p>
                 <p className="text-lg">
                   Below are a few technologies I have been working with recently:
@@ -98,11 +102,11 @@ export default function About() {
               </div>
 
               {/* Tech stack */}
-              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 mt-7">
+              <div className="flex flex-wrap justify-center mt-7">
                 {technologies.map((technology, i) => (
                   <div
                     key={i}
-                    className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-indigo-600 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                    className="relative w-2/5 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-indigo-600 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500 m-3 mb-3"
                   >
                     <div className="flex-1 min-w-0">
                       <span className="absolute inset-0" aria-hidden="true" />
