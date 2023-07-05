51c87c9b0ec7b08ba139a481e47c162fa0abc9b7
edytatar <edytatar@gmail.com>
2023-07-05 16:53:56 -0700

    Added routes, project pages, and reconfigured App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
-
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./components/Homepage";
 import Nav from './components/Nav'
 import Footer from './components/Footer'
-import Hero from './components/Hero'
-import About from './components/About'
-import Project from './components/Project'
-import Contact from './components/Contact'
+import Polari from "./pages/Polari";
+import Neighorgoods from "./pages/Neighborgoods";
 
-import Polari from './pages/Polari'
-import Neighborgoods from './pages/Neighborgoods'
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Hero />
-      <About />
-      <Project />
       <Routes>
-        <Route>path='/polari' element={<Polari />} </Route>
-        <Route>path='/neighborgoods' element={<Neighborgoods />}</Route>
-      </Routes>
-      <Contact />
+          <Route path='/' element={<Homepage />} />
+          <Route path='/polari' element={<Polari />} />
+          <Route path='/neighorgoods' element={<Neighorgoods />} />
+        </Routes>
       <Footer />
     </div>
   );
