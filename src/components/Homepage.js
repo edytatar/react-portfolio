772171e14f9db2f3957dbd521e8b1bc2a3a5f7be
edytatar <edytatar@gmail.com>
2023-07-05 16:54:10 -0700

    Added a homepage component

---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import Hero from './Hero';
+import About from './About';
+import Project from './Project';
+import Contact from './Contact';
+
+export default function Homepage() {
+    return (
+        <div>
+            <Hero />
+            <About />
+            <Project />
+            <Contact />
+        </div>
+    )
+}
