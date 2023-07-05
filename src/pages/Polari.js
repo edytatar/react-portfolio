4f1f1b2338796e51e248757fd56463e66afe3194
edytatar <edytatar@gmail.com>
2023-07-05 16:00:48 -0700

    Adding Polari page dummy text

--- src/pages/Polari.js
+++ src/pages/Polari.js
@@ -1,6 +1,17 @@
 export default function Polari() {
-    return (
-      
-    )
-  }
-  
+  return (
+    <div id="polari" className="bg-white">
+      {/* Project Header */}
+      <div>
+        <div className="bg-transparent max-w-7xl mx-auto py-10 px-4 sm:py-25 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">2.</h2>
+            <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+              Projects
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
